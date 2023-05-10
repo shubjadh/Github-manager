431225514bbe1e8f69e2285429278fa105ef94a2
shubjadh <shubjadh20000@gmail.com>
2023-05-10 17:47:42 -0400

    newfinal 2

---
+++ basic-project_2/src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <p> This is the Home pages</p>
+    
+    
+  </template>
+    
+    <script lang="ts">
+    export default {
+      name: 'HomePage',
+    };
+    </script>
+    
+    <script setup lang="ts">
+    //Most code goes here
+    </script>
+    
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped></style>
